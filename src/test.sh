952ecc1ee5c81e4085f4bee9988393bedb5965f5
HaatoWatanabe <heart.watanabe2000@gmail.com>
2025-07-29 10:46:37 +0900

    modified test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,19 +1,62 @@
+RED="\033[31m"
+GREEN="\033[32m"
+RESET="\033[0m"
+OK=$GREEN"OK"$RESET
+NG=$RED"NG"$RESET
+
+cat << EOF | cc -xc -o a.out -
+#include <stdio.h>
+int main() { printf("hello from a.out\n"); }
+EOF
+
+cat << EOF | cc -xc -o print_args -
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  for (int i = 0; argv[i]; i++)
+    printf("argv[%d] = %s\n", i, argv[i]);
+}
+EOF
+
+cat << EOF | cc -xc -o exit42 -
+#include <stdio.h>
+int main() { return 42; }
+EOF
+
+cleanup() {
+  rm -f cmp out a.out print_args exit42
+}
+
 assert() {
-  printf '%-30s:' "\"$1\""
-
-  echo -n -e "$1" | bash >cmp 2>&-
+  COMMAND="$1"
+  shift
+  printf '%-30s:' "[$COMMAND]"
+  # exit status
+  echo -n -e "$COMMAND" | bash >cmp 2>&-
   expected=$?
-
-  echo -n -e "$1" | ./minishell >out 2>&-
+  for arg in "$@"
+  do
+    mv "$arg" "$arg"".cmp"
+  done
+  echo -n -e "$COMMAND" | ./minishell >out 2>&-
   actual=$?
+  for arg in "$@"
+  do
+    mv "$arg" "$arg"".out"
+  done
 
-  diff cmp out >/dev/null && echo -n ' . diff OK' || echo -n '  diff NG'
+  diff cmp out >/dev/null && echo -e -n " diff $OK" || echo -e -n "  diff $NG"
 
   if [ "$actual" = "$expected" ]; then
-    echo -n ' . status OK'
+    echo -e -n " status $OK"
   else
-    echo -n "  status NG, expected $expected but got $actual"
+    echo -e -n " status $NG, expected $expected but got $actual"
   fi
+  for arg in "$@"
+  do
+    echo -n " [$arg]"
+    diff "$arg"".cmp" "$arg"".out" >/dev/null && echo -e -n "$OK" || echo -e -n "$NG"
+    rm -f "$arg"".cmp" "$arg"".out"
+  done
   echo
 }
 
@@ -31,4 +74,73 @@ assert 'echo'
 assert 'ls'
 assert './a.out'
 
-rm cmp out
+## no such command
+assert 'a.out'
+assert 'nosuchfile'
+
+# Tokenize
+## unquoted word
+assert 'ls /'
+assert 'echo hello  world '
+assert 'nosuchfile\n\n'
+
+## single quote
+assert "./printf_args 'hello  world'  '42Tokyo'"
+assert "echo  'hello  world'  '42Tokyo'"
+assert "echo  '\"hello  world\"'  '42Tokyo'"
+
+## double quote
+assert './printf_args "hello  world"  "42Tokyo"'
+assert 'echo  "hello  world"  "42Tokyo"'
+assert "echo  \"'hello  world'\"  \"42Tokyo\""
+
+## combination
+assert "echo hello' world'"
+assert "echo hello' world '\" 42Tokyo \""
+
+# Redirect
+## Redirecting output
+assert 'echo hello >hello.txt' 'hello.txt'
+assert 'echo hello >f1>f2>f3' 'f1' 'f2' 'f3'
+
+## Redirecting input
+assert 'cat <Makefile'
+echo hello >f1
+echo world >f2
+echo 42Tokyo >f3
+assert 'cat <f1<f2<f3'
+rm -f f1 f2 f3
+assert 'cat <hoge'
+
+## Appending Redirected output
+assert 'pwd >>pwd.txt' 'pwd.txt'
+assert 'pwd >>pwd.txt \n pwd >> pwd.txt' 'pwd.txt'
+
+## Here Document
+assert 'cat <<EOF\nhello\nworld\nEOF\nNOPRINT'
+assert 'cat <<EOF<<eof\nhello\nworld\nEOF\neof\nNOPRINT'
+assert 'cat <<EOF\nhello\nworld'
+assert 'cat <<E"O"F\nhello\nworld\nEOF\nNOPRINT'
+assert 'cat <<EOF \n$USER\n$NO_SUCH_VAR\n$FOO$BAR\nEOF'
+assert 'cat <<"EOF" \n$USER\n$NO_SUCH_VAR\n$FOO$BAR\nEOF'
+assert 'cat <<EO"F" \n$USER\n$NO_SUCH_VAR\n$FOO$BAR\nEOF'
+export EOF="eof"
+assert 'cat <<$EOF \neof\n$EPF\nEOF'
+assert 'cat <<"$EOF" \neof\n$EPF\nEOF'
+
+# Pipe
+assert 'cat Makefile | grep minishell'
+assert 'cat | cat |ls\n\n'
+
+# Expand
+assert 'echo $USER'
+assert 'echo $USER$PATH$TERM'
+assert 'echo "$USER $PATH $TERM"'
+
+# Special Parameter $?
+assert 'echo $?'
+assert 'invalid\necho $?\necho $?'
+assert 'exit42\necho $?\necho $?'
+assert 'exit42\n\necho $?\necho $?'
+
+cleanup
