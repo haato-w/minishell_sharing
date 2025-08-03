e1c7e82269748f5413caffad8f45166bbc248247
HaatoWatanabe <heart.watanabe2000@gmail.com>
2025-08-03 20:33:45 +0900

    modified builtin exit

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -23,23 +23,6 @@ bool is_numeric(char *s) // 数値かどうかをチェック
     return (1); // 数値の場合は1を返す
 }
 
-// シンプルなエラー出力関数（dprintfが使えなかった
-void error_msg(char *msg)
-{
-    size_t len = strlen(msg);
-    ssize_t ret = write(STDERR_FILENO, msg, len);
-    (void)ret; // 戻り値を変数に受け取ってから無視
-}
-
-// フォーマット付きエラー出力関数
-void error_msg_fmt(char *prefix, char *arg, char *suffix)
-{
-    error_msg(prefix);
-    if (arg)
-        error_msg(arg);
-    error_msg(suffix);
-}
-
 int builtin_exit(char **argv)
 {
     int exit_code;
@@ -47,25 +30,25 @@ int builtin_exit(char **argv)
     exit_code = last_status;
     if (syntax_error)
     {
-        error_msg("syntax error\n");
+        xperror("syntax error\n");
         return (1);
     }
     if (argv[1] != NULL)
     {
         if (argv[2] != NULL) // 引数が2つ以上ある場合
         {
-            error_msg("exit: too many arguments\n");
+            xperror("exit: too many arguments\n");
             exit(1); // 引数が２つ以上ある場合、本家も１を返す
         }
         if (!is_numeric(argv[1])) // 数値でない場合
         {
-            error_msg_fmt("exit: ", argv[1], ": numeric argument required\n");
+            builtin_error("exit: ", argv[1], ": numeric argument required\n");
             exit(255);
         }
         exit_code = atoi(argv[1]); // 引数が数値の場合そのままexit_codeに設定
         // bashと同じ動作にするため、負数の場合も正しく0-255の範囲に収める
         exit_code = ((exit_code % 256) + 256) % 256;
     }
-    error_msg("exit\n");
+    xperror("exit\n");
     exit(exit_code); // シェルを終了させる
 }
