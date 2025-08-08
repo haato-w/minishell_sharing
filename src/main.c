eb9b109d3de6b325a21f854b2a0fc224802c5140
heart <heart.watanabe2000@gmail.com>
2025-08-09 04:53:00 +0900

    applied norm to main.c

--- src/main.c
+++ src/main.c
@@ -1,49 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heart <heart@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 04:48:10 by heart             #+#    #+#             */
+/*   Updated: 2025/08/09 04:52:33 by heart            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-t_context g_ctx = {};
+t_context	g_ctx = {};
 
-void interpret(char *line, int *stat_loc)
+void	interpret(char *line, int *stat_loc)
 {
-  t_token *tok;
-  t_node *node;
+	t_token	*tok;
+	t_node	*node;
 
-  tok = tokenize(line);
-  if (at_eof(tok))
-    ;
-  else if (g_ctx.syntax_error)
-    *stat_loc = ERROR_TOKENIZE;
-  else
-  {
-    node = parse(tok);
-    if (g_ctx.syntax_error)
-      *stat_loc = ERROR_PARSE;
-    else
-    {
-      expand(node);
-      *stat_loc = exec(node);
-    }
-    free_node(node);
-  }
-  free_tok(tok);
+	tok = tokenize(line);
+	if (at_eof(tok))
+		;
+	else if (g_ctx.syntax_error)
+		*stat_loc = ERROR_TOKENIZE;
+	else
+	{
+		node = parse(tok);
+		if (g_ctx.syntax_error)
+			*stat_loc = ERROR_PARSE;
+		else
+		{
+			expand(node);
+			*stat_loc = exec(node);
+		}
+		free_node(node);
+	}
+	free_tok(tok);
 }
 
-int main(void)
+int	main(void)
 {
-  char *line;
+	char	*line;
 
-  rl_outstream = stderr;
-  initenv();
-  setup_signal();
-  g_ctx.last_status = 0;
-  while (1)
-  {
-    line = readline("minishell$ ");
-    if (line == NULL)
-      break;
-    if (*line)
-      add_history(line);
-    interpret(line, &g_ctx.last_status);
-    free(line);
-  }
-  return (g_ctx.last_status);
+	rl_outstream = stderr;
+	initenv();
+	setup_signal();
+	g_ctx.last_status = 0;
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (line == NULL)
+			break ;
+		if (*line)
+			add_history(line);
+		interpret(line, &g_ctx.last_status);
+		free(line);
+	}
+	return (g_ctx.last_status);
 }
