4c17740f5a418e7a51de60ff9fc5d72bab089b79
HaatoWatanabe <heart.watanabe2000@gmail.com>
2025-08-09 18:50:54 +0900

    applied norm

--- src/utils/error.c
+++ src/utils/error.c
@@ -2,70 +2,70 @@
 
 #define ERROR_PREFIX "minishell: "
 
-static void perror_prefix(void)
+static void	perror_prefix(void)
 {
-  dprintf(STDERR_FILENO, "%s", ERROR_PREFIX);
+	dprintf(STDERR_FILENO, "%s", ERROR_PREFIX);
 }
 
-void fatal_error(const char *msg)
+void	fatal_error(const char *msg)
 {
-  perror_prefix();
-  dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
-  exit(1);
+	perror_prefix();
+	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
+	exit(1);
 }
 
-void assert_error(const char *msg)
+void	assert_error(const char *msg)
 {
-  perror_prefix();
-  dprintf(STDERR_FILENO, "Assert Error: %s\n", msg);
-  exit(255); // 255?
+	perror_prefix();
+	dprintf(STDERR_FILENO, "Assert Error: %s\n", msg);
+	exit(255);
 }
 
-void err_exit(const char *location, const char *msg, int status)
+void	err_exit(const char *location, const char *msg, int status)
 {
-  perror_prefix();
-  dprintf(STDERR_FILENO, "%s: %s\n", location , msg);
-  exit(status);
+	perror_prefix();
+	dprintf(STDERR_FILENO, "%s: %s\n", location , msg);
+	exit(status);
 }
 
-void todo(const char *msg)
+void	todo(const char *msg)
 {
-  perror_prefix();
-  dprintf(STDERR_FILENO, "TODO: %s\n", msg);
-  exit(255);
+	perror_prefix();
+	dprintf(STDERR_FILENO, "TODO: %s\n", msg);
+	exit(255);
 }
 
-void tokenize_error(const char *location, char **rest, char *line)
+void	tokenize_error(const char *location, char **rest, char *line)
 {
-  g_ctx.syntax_error = true;
-  perror_prefix();
-  dprintf(STDERR_FILENO, "syntax errror near %s\n", location);
-  while (*line)
-    line++;
-  *rest = line;
+	g_ctx.syntax_error = true;
+	perror_prefix();
+	dprintf(STDERR_FILENO, "syntax errror near %s\n", location);
+	while (*line)
+	line++;
+	*rest = line;
 }
 
-void parse_error(const char *location, t_token **rest, t_token *tok)
+void	parse_error(const char *location, t_token **rest, t_token *tok)
 {
-  g_ctx.syntax_error = true;
-  perror_prefix();
-  dprintf(STDERR_FILENO, "syntax error near unexpected token `%s' in %s\n", tok->word, location);
-  while (tok && !at_eof(tok))
-    tok = tok->next;
-  *rest = tok;
+	g_ctx.syntax_error = true;
+	perror_prefix();
+	dprintf(STDERR_FILENO, "syntax error near unexpected token `%s' in %s\n", tok->word, location);
+	while (tok && !at_eof(tok))
+		tok = tok->next;
+	*rest = tok;
 }
 
-void xperror(const char *locaton)
+void	xperror(const char *locaton)
 {
-  perror_prefix();
-  perror(locaton);
+	perror_prefix();
+	perror(locaton);
 }
 
-void builtin_error(const char *func, const char *name, const char *err)
+void	builtin_error(const char *func, const char *name, const char *err)
 {
-  perror_prefix();
-  dprintf(STDERR_FILENO, "%s: ", func);
-  if (name)
-    dprintf(STDERR_FILENO, "`%s': ", name);
-  dprintf(STDERR_FILENO, "%s\n", err);
+	perror_prefix();
+	dprintf(STDERR_FILENO, "%s: ", func);
+	if (name)
+		dprintf(STDERR_FILENO, "`%s': ", name);
+	dprintf(STDERR_FILENO, "%s\n", err);
 }
