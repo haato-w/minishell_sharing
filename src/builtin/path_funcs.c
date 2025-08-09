8528c065ee9fe42b0754d59d0ee05b022d3d7f7a
Haato Watanabe <haatwata@c6r1s12.42tokyo.jp>
2025-08-09 16:27:01 +0900

    applied norm to path_funcs.c

--- src/builtin/path_funcs.c
+++ src/builtin/path_funcs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   path_funcs.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heart <heart@student.42.fr>                +#+  +:+       +#+        */
+/*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 14:00:38 by heart             #+#    #+#             */
-/*   Updated: 2025/08/09 14:35:41 by heart            ###   ########.fr       */
+/*   Updated: 2025/08/09 16:26:28 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,42 +45,42 @@ void	delete_last_elem(char *path)
 		*last_slash_ptr = '\0';
 }
 
-size_t strlncpy(char *dst, char *src, size_t n, size_t dstsize)
+size_t	strlncpy(char *dst, char *src, size_t n, size_t dstsize)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (i < dstsize  - 1 && n > 0 && *src)
-    {
-        dst[i] = *src;
-        src++;
-        i++;
-        n--;
-    }
-    dst[i] = '\0';
-    while (n > 0 && *src)
-    {
-        i++;
-        n--;
-        src++;
-    }
-    return (i);
+	i = 0;
+	while (i < dstsize - 1 && n > 0 && *src)
+	{
+		dst[i] = *src;
+		src++;
+		i++;
+		n--;
+	}
+	dst[i] = '\0';
+	while (n > 0 && *src)
+	{
+		i++;
+		n--;
+		src++;
+	}
+	return (i);
 }
 
 void	append_path_elem(char *dst, size_t dstsize, char **rest, char *src)
 {
-    char buf[PATH_MAX];
-    size_t	elem_len;
+	char	buf[PATH_MAX];
+	size_t	elem_len;
 
 	elem_len = 0;
 	while (src[elem_len] && src[elem_len] != '/')
 		elem_len++;
-    if (strlncpy(buf, src, elem_len, PATH_MAX) >= PATH_MAX)
-        assert_error("strlncpy error");
-    if (dst[ft_strlen(dst) - 1] != '/')
-        if (ft_strlcat(dst, "/", dstsize) >= dstsize)
-            assert_error("This will not happen");
-    if (ft_strlcat(dst, buf, dstsize) >= dstsize)
-        assert_error("ft_strlcat error");
-    *rest = src + elem_len;
+	if (strlncpy(buf, src, elem_len, PATH_MAX) >= PATH_MAX)
+		assert_error("strlncpy error");
+	if (dst[ft_strlen(dst) - 1] != '/')
+		if (ft_strlcat(dst, "/", dstsize) >= dstsize)
+			assert_error("This will not happen");
+	if (ft_strlcat(dst, buf, dstsize) >= dstsize)
+		assert_error("ft_strlcat error");
+	*rest = src + elem_len;
 }
