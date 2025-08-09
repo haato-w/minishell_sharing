3cdea558df18ddf530ace28aef71b4f6398e8fc4
Haato Watanabe <haatwata@c6r1s12.42tokyo.jp>
2025-08-09 16:29:40 +0900

    apllied norm to addtional fts

--- src/utils/ft_xstrdup.c
+++ src/utils/ft_xstrdup.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_xstrdup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heart <heart@student.42.fr>                +#+  +:+       +#+        */
+/*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 05:46:42 by heart             #+#    #+#             */
-/*   Updated: 2025/08/09 05:55:12 by heart            ###   ########.fr       */
+/*   Updated: 2025/08/09 16:29:05 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char *ft_xstrdup(const char *s1)
+char	*ft_xstrdup(const char *s1)
 {
-    char    *ret;
-    
-    ret = ft_strdup(s1);
-    if (ret == NULL)
-        fatal_error("xstrdup");
-    return (ret);
+	char	*ret;
+
+	ret = ft_strdup(s1);
+	if (ret == NULL)
+		fatal_error("xstrdup");
+	return (ret);
 }
--- src/utils/ft_xstrndup.c
+++ src/utils/ft_xstrndup.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_xstrndup.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heart <heart@student.42.fr>                +#+  +:+       +#+        */
+/*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 05:46:42 by heart             #+#    #+#             */
-/*   Updated: 2025/08/09 05:55:02 by heart            ###   ########.fr       */
+/*   Updated: 2025/08/09 16:28:18 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char *ft_xstrndup(const char *s1, size_t size)
+char	*ft_xstrndup(const char *s1, size_t size)
 {
-    char    *ret;
-    
-    ret = ft_strndup(s1, size);
-    if (ret == NULL)
-        fatal_error("xstrndup");
-    return (ret);
+	char	*ret;
+
+	ret = ft_strndup(s1, size);
+	if (ret == NULL)
+		fatal_error("xstrndup");
+	return (ret);
 }
