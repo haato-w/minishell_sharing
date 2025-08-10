6273e9e7e013fa6c66c728522084a9eba385568c
Haato Watanabe <haatwata@c6r1s10.42tokyo.jp>
2025-08-10 18:20:46 +0900

    modified search_path

--- src/exec/search_path.c
+++ src/exec/search_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   search_path.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heart <heart@student.42.fr>                +#+  +:+       +#+        */
+/*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 05:29:53 by heart             #+#    #+#             */
-/*   Updated: 2025/08/09 05:32:41 by heart            ###   ########.fr       */
+/*   Updated: 2025/08/10 18:17:53 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,15 @@ static char	*search_path_mode(const char *filename, int mode)
 	char	path[PATH_MAX];
 	char	*envpath;
 	char	*dup;
+	char	*tmp;
 
 	envpath = xgetenv("PATH");
+	if (envpath == NULL)
+	{
+		tmp = ft_strjoin("./", filename);
+		if (access(tmp, mode) == 0)
+			return (tmp);
+	}
 	while (envpath && *envpath)
 	{
 		set_path(path, PATH_MAX, filename, &envpath);
