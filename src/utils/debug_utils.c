2546536851cedd2d55e591b889793caab4e324df
HaatoWatanabe <heart.watanabe2000@gmail.com>
2025-08-23 18:18:43 +0900

    updated utils/debug_utils.c

--- src/utils/debug_utils.c
+++ src/utils/debug_utils.c
@@ -6,34 +6,109 @@
 /*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 18:59:14 by haatwata          #+#    #+#             */
-/*   Updated: 2025/08/09 18:59:14 by haatwata         ###   ########.fr       */
+/*   Updated: 2025/08/23 18:15:02 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// void show_token(t_token *tok)
-// {
-//   t_token *tmp = tok;
-//   while (tmp && tmp->kind != TK_EOF)
-//   {
-//     printf("kind: %u, word: %s\n", tmp->kind, tmp->word);
-//     tmp = tmp->next;
-//   }
-// }
+static void print_tab(int num)
+{
+  size_t i;
+  
+  i = 0;
+  while (i < num)
+  {
+    printf("\t");
+    i++;
+  }
+}
 
-// void show_node(t_node *node)
-// {
-//   t_node *tmp = node;
-//   while (tmp != NULL)
-//   {
-//     printf("show_node kind: %u\n", tmp->kind);
-//     printf("token\n");
-//     show_token(node->args);
-//     printf("redirects\n");
-//     show_node(node->redirects);
-//     printf("filename\n");
-//     show_token(node->filename);
-//     tmp = tmp->next;
-//   }
-// }
+void show_token(t_token *tok, int level)
+{
+  t_token *tmp = tok;
+  
+  while (tmp && tmp->kind != TK_EOF)
+  {
+    print_tab(level);
+    printf("==show token==\n");
+    print_tab(level);
+    printf("kind: %u", tmp->kind);
+    if (tmp->kind == TK_WORD)
+      printf("\tTK_WORD\n");
+    else if (tmp->kind == TK_RESERVED)
+      printf("\tTK_RESERVED\n");
+    else if (tmp->kind == TK_OP)
+      printf("\tTK_OP\n");
+    else if (tmp->kind == TK_EOF)
+      printf("\tTK_EOF\n");
+    else
+      printf("\ttoke kind error!!\n");
+    print_tab(level);
+    printf("word: %s\n", tmp->word);
+    tmp = tmp->next;
+  }
+}
+
+void show_node(t_node *node, int level)
+{
+  t_node *tmp = node;
+  
+  while (tmp != NULL)
+  {
+    print_tab(level);
+    printf("===show node===\n");
+    print_tab(level);
+    printf("kind: %u\t", tmp->kind);
+    if (tmp->kind == 0)
+      printf("ND_PIPELINE\n");
+    else if (tmp->kind == 1)
+      printf("ND_SIMPLE_CMD\n");
+    else if (tmp->kind == 2)
+      printf("ND_REDIR_OUT\n");
+    else if (tmp->kind == 3)
+      printf("ND_REDIR_IN\n");
+    else if (tmp->kind == 4)
+      printf("ND_SIMPLE_CMD\n");
+    else if (tmp->kind == 5)
+      printf("ND_REDIR_APPEND\n");
+    else if (tmp->kind == 6)
+      printf("ND_REDIR_HEREDOC\n");
+    
+    print_tab(level);
+    printf("token\n");
+    show_token(node->args, level + 1);
+    print_tab(level);
+    printf("redirects\n");
+    show_node(node->redirects, level + 1);
+    print_tab(level);
+    printf("filename\n");
+    show_token(node->filename, level + 1);
+    if (node->command)
+    {
+      print_tab(level);
+      printf("command\n");
+      print_tab(level);
+      printf("=====\n");
+      show_node(node->command, level + 1);
+      print_tab(level);
+      printf("=====\n");
+    }
+    else
+    {
+      print_tab(level);
+      printf("no command\n");
+    }
+    if (tmp->next)
+    {
+      print_tab(level);
+      printf("has next!\n\n");
+    }
+    else
+    {
+      print_tab(level);
+      printf("no next\n");
+    }
+    tmp = tmp->next;
+  }
+}
