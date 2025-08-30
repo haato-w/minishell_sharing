aad311779987915f546f862e845b7f4af6fc3567
Haato Watanabe <haatwata@c6r3s9.42tokyo.jp>
2025-08-30 14:33:56 +0900

    modified buintin cd

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -6,37 +6,11 @@
 /*   By: haatwata <haatwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 16:10:32 by haatwata          #+#    #+#             */
-/*   Updated: 2025/08/27 20:29:23 by haatwata         ###   ########.fr       */
+/*   Updated: 2025/08/30 14:27:31 by haatwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-static char	*resolve_pwd(char *oldpwd, char *path)
-{
-	char	newpwd[PATH_MAX];
-	char	*dup;
-
-	if (*path == '/' || oldpwd == NULL)
-		ft_strlcpy(newpwd, "/", PATH_MAX);
-	else
-		ft_strlcpy(newpwd, oldpwd, PATH_MAX);
-	while (*path)
-	{
-		if (*path == '/')
-			path++;
-		else if (consume_path(&path, path, "."))
-			;
-		else if (consume_path(&path, path, ".."))
-			delete_last_elem(newpwd);
-		else
-			append_path_elem(newpwd, PATH_MAX, &path, path);
-	}
-	dup = ft_strdup(newpwd);
-	if (dup == NULL)
-		fatal_error("strdup");
-	return (dup);
-}
 
 static void	update_oldpwd(char *pwd)
 {
@@ -67,12 +41,12 @@ static int	set_path(char *path, size_t path_size, char *arg)
 
 int	builtin_cd(char **argv)
 {
-	char	*pwd;
+	char	*env_pwd;
+	char	pwd[PATH_MAX];
 	char	path[PATH_MAX];
-	char	*newpwd;
 
-	pwd = xgetenv("PWD");
-	update_oldpwd(pwd);
+	env_pwd = xgetenv("PWD");
+	update_oldpwd(env_pwd);
 	if (set_path(path, PATH_MAX, argv[1]) < 0)
 		return (1);
 	if (chdir(path) < 0)
@@ -80,9 +54,9 @@ int	builtin_cd(char **argv)
 		xperror3("cd", path, NULL);
 		return (1);
 	}
-	newpwd = resolve_pwd(pwd, path);
+	if (getcwd(pwd, PATH_MAX) == NULL)
+		fatal_error("getcwd");
 	if (map_get((*get_ctx()).envmap, "PWD") != NULL)
-		map_set((*get_ctx()).envmap, "PWD", newpwd);
-	free(newpwd);
+		map_set((*get_ctx()).envmap, "PWD", pwd);
 	return (0);
 }
